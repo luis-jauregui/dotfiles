24a9a00947330aa1a78ef1db7a43e76405299710
Luis Enrique <lu.enriquejv@gmail.com>
2022-10-17 18:40:13 -0500

    chore(autocmd): more autocmds

--- after/plugin/autocmds.lua
+++ after/plugin/autocmds.lua
@@ -1,11 +1,33 @@
 local api = vim.api
 
+-- go to last loc when opening a buffer
+
 api.nvim_create_autocmd(
   "BufReadPost",
   { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
 
+-- highlight on yank
+
+local yank = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yank
+})
+
+
+-- show diagnostics when hover it
+
+-- vim.o.updatetime = 250
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus = false})]]
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * Lspsaga show_line_diagnostics]]
+
+-- don't auto comment new line
+
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
 -- windows to close with q
+
 api.nvim_create_autocmd("FileType", {
   pattern = {
     "help",
@@ -26,3 +48,8 @@ api.nvim_create_autocmd("FileType", {
 
 api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
 api.nvim_create_autocmd("FileType", { pattern = "cheat", command = [[nnoremap <buffer><silent> q :bdelete!<CR>]] })
+
+-- vim.cmd [[
+--   set completeopt=menuone,noinsert,noselect
+--   highlight! default link CmpItemKind CmpItemMenuDefault
+-- ]]
